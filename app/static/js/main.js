0267c3991886398b6c2c40612004ff863e254671
JacobG <jaqobek1995@gmail.com>
2021-07-07 17:54:43 +0200

    localhost to window.location

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,7 +2,7 @@
 var users_data = []
 const getDefaultData = async () => {
     try {
-        const response = await fetch('http://127.0.0.1:5000/api/users')
+        const response = await fetch(window.location.href + '/api/users')
         const data = await response.json()
         // Call function to render users inside
         for (let i = 0; i < data.length; i++) {
